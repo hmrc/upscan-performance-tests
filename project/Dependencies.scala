49459d02d8190660a4e0605b9dc522588bea3b44
jordanrowe <37838443+jordanrowe@users.noreply.github.com>
2024-12-05 11:49:07 +0000

    BDOG-3254 update plugins and libraries, set polling timeout to 90s to match README

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,12 +3,8 @@ import sbt._
 object Dependencies {
 
   val test = Seq(
-    "io.gatling"             % "gatling-test-framework"    % "3.5.1"  % Test,
-    "io.gatling.highcharts"  % "gatling-charts-highcharts" % "3.5.1"  % Test,
-    "com.typesafe"           % "config"                    % "1.3.0"  % Test,
-    "uk.gov.hmrc"            %% "performance-test-runner"  % "5.6.0"  % Test,
-    "io.gatling.vtd"         % "gatling-vtd"               % "2.2.0"  % Test,
-    "org.json4s"             %% "json4s-jackson"           % "4.0.7" % Test,
-    "org.json4s"             %% "json4s-native"            % "4.0.7" % Test
+    "uk.gov.hmrc"  %% "performance-test-runner"  % "6.1.0" % Test,
+    "org.json4s"   %% "json4s-jackson"           % "4.0.7" % Test,
+    "org.json4s"   %% "json4s-native"            % "4.0.7" % Test
   )
 }
